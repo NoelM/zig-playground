eadc96d66bec211609cdb13eb1ad80531d9559ff
Noel Martin <noel@noelmrtn.fr>
2023-08-05 11:24:38 +0200

    async has not been implemented...

--- prime_numbers_parallel/primep.zig
+++ prime_numbers_parallel/primep.zig
@@ -1,21 +1,37 @@
 const std = @import("std");
 
-const Pipe = struct {
-    mtx: std.Thread.Mutex,
-    arr: []u64,
-    max: u32,
-    pos: u32,
+const i64Chan = std.event.Channel(i64);
 
-    pub fn init() Pipe {
-        return Pipe{
-            .arr = &[_]u64{0} ** 10,
-            .max = 10,
-            .pos = 10,
-        };
+pub fn isPrime(quit: *bool, toComputeChan: *i64Chan, computedChan: *i64Chan) bool {
+    while (!*quit) {
+        const int = await toComputeChan.get();
+        var i: u64 = 2;
+        while (i < int) : (i += 1) {
+            if (int % i == 0) {
+                break;
+            }
+        }
+        computedChan.put(int);
     }
-};
+}
 
 pub fn main() !void {
-    const p = Pipe;
-    _ = p.init();
+    const toComputeBuf = []i64;
+    const toComputeChan = std.event.Channel(i64);
+    toComputeChan.init(toComputeBuf);
+
+    const computedBuf = []i64;
+    const computedChan = std.event.Channel(i64);
+    computedChan.init(computedBuf);
+
+    var quit = false;
+    var config = std.Thread.SpawnConfig{};
+    std.Thread.spawn(config, isPrime, &quit, &toComputeChan, &computedChan);
+
+    var value: i64 = 0;
+    while (true) {
+        toComputeChan.put(value);
+        const c = await computedChan.get();
+        std.log.debug("prime: %d", .{c});
+    }
 }
