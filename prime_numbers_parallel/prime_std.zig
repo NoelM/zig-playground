e165169647d32cb38bb97cc9465394c89bbfc299
Noel Martin <noel@noelmrtn.fr>
2023-08-15 22:51:36 +0200

    std lib implem

--- prime_numbers_parallel/prime_std.zig
+++ prime_numbers_parallel/prime_std.zig
@@ -5,28 +5,38 @@ const Node = U64Queue.Node;
 const Pool = std.Thread.Pool;
 const WaitGroup = std.Thread.WaitGroup;
 
-pub fn isPrimeRoutine(quit: *bool, wait_group: *WaitGroup, int_to_test: *U64Queue, int_prime: *U64Queue) void {
+pub fn isPrimeRoutine(wait_group: *WaitGroup, allocator: *const std.mem.Allocator, start: u64, shard_size: u64, int_prime: *U64Queue) void {
     wait_group.start();
     defer wait_group.finish();
 
-    while (!quit.*) {
-        if (int_to_test.get()) |node| {
-            const value = node.data;
+    for (start..start + shard_size) |value| {
+        if (value == 0) {
+            continue;
+        }
 
-            var is_value_prime = true;
-            var i: u64 = 2;
-            while (i < value) : (i += 1) {
-                if (value % i == 0) {
-                    is_value_prime = false;
-                    break;
-                }
+        var is_value_prime = true;
+        var i: u64 = 2;
+        while (i < value) : (i += 1) {
+            if (value % i == 0) {
+                is_value_prime = false;
+                break;
             }
+        }
 
-            if (is_value_prime) {
-                node.prev = undefined;
-                node.next = undefined;
-                int_prime.put(node);
-            }
+        const node: *Node = allocator.create(Node) catch |err| {
+            std.log.err("cannot allocate for int: {d}, error: {}\n", .{ value, err });
+            continue;
+        };
+        node.* = .{
+            .prev = undefined,
+            .next = undefined,
+            .data = value,
+        };
+
+        if (is_value_prime) {
+            node.prev = undefined;
+            node.next = undefined;
+            int_prime.put(node);
         }
     }
 }
@@ -40,46 +50,25 @@ pub fn main() !void {
     };
     const arena = thread_safe_arena.allocator();
 
-    const cpu_count: u32 = @intCast(try std.Thread.getCpuCount());
-
     var thread_pool: Pool = undefined;
     try thread_pool.init(Pool.Options{
         .allocator = arena,
-        .n_jobs = cpu_count,
     });
     defer thread_pool.deinit();
 
-    var int_to_test = U64Queue.init();
     var int_prime = U64Queue.init();
 
     var wait_group: WaitGroup = undefined;
     wait_group.reset();
 
-    var quit = false;
-    var thread_id: u32 = 0;
-    while (thread_id < cpu_count) : (thread_id += 1) {
-        thread_pool.spawn(isPrimeRoutine, .{ &quit, &wait_group, &int_to_test, &int_prime }) catch |err| {
-            return err;
-        };
-    }
-
     const value_max: u64 = 1000;
-    var value: u64 = 1;
-    while (value < value_max) {
-        if (int_to_test.isEmpty()) {
-            const node: *Node = try arena.create(Node);
-            node.* = .{
-                .prev = undefined,
-                .next = undefined,
-                .data = value,
-            };
-            int_to_test.put(node);
-            value += 1;
-        }
-    }
+    const shard_size: u64 = 10;
 
-    quit = true;
-    wait_group.wait();
+    var value: u64 = 0;
+    while (value < value_max) : (value += shard_size) {
+        try thread_pool.spawn(isPrimeRoutine, .{ &wait_group, &arena, value, shard_size, &int_prime });
+    }
+    thread_pool.waitAndWork(&wait_group);
 
     const file = try std.fs.cwd().createFile(
         "prime_numbers.txt",
